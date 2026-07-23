c91ccd0e591fb47da1a528025e0654c47eb3d91d
Anildagar55 <anildagaranildagar@gmail.com>
2026-07-23 22:16:38 +0530

    first commit

---
+++ src/test/java/com/example/Eco_Backend/EcoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Eco_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
